645d2c235d859fc2570d4ec6410a5301df27c54e
Igor Vartic <viplog@pm.me>
2025-12-05 10:11:20 +0000

    Update insertion of header and footer

--- src/main.js
+++ src/main.js
@@ -1,28 +1,5 @@
 import './style.css'
 
-
-// Injecting header and footer partials
-const loadHtml = async (selector, url) => {
-  const element = document.querySelector(selector);
-  if (element) {
-    const response = await fetch(url);
-    if (response.ok) {
-      const text = await response.text();
-      element.innerHTML = text;
-
-      if (selector === '#header-placeholder') {
-        initializeMobileMenu();
-      }
-    }
-  }
-};
-
-// Load Header and Footer
-document.addEventListener("DOMContentLoaded", () => {
-  loadHtml("#header-placeholder", "/partials/_header.html");
-  loadHtml("#footer-placeholder", "/partials/_footer.html");
-});
-
 // Mobile Menu Toggle
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
