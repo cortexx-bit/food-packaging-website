3278cb9655ac43615eaa0da942606792ff6634f3
Igor Vartic <viplog@pm.me>
2025-12-05 00:44:54 +0000

    Add header and footer

--- src/main.js
+++ src/main.js
@@ -1,30 +1,7 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
 
 
-// Function for injecting header and footer partials
+// Injecting header and footer partials
 const loadHtml = async (selector, url) => {
   const element = document.querySelector(selector);
   if (element) {
@@ -32,11 +9,37 @@ const loadHtml = async (selector, url) => {
     if (response.ok) {
       const text = await response.text();
       element.innerHTML = text;
+
+      if (selector === '#header-placeholder') {
+        initializeMobileMenu();
+      }
     }
   }
 };
 
+// Load Header and Footer
 document.addEventListener("DOMContentLoaded", () => {
   loadHtml("#header-placeholder", "/partials/_header.html");
   loadHtml("#footer-placeholder", "/partials/_footer.html");
 });
+
+// Mobile Menu Toggle
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+if (mobileMenuButton) {
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+}
+
+const initializeMobileMenu = () => {
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  if (mobileMenuButton) {
+    mobileMenuButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+  }
+};
